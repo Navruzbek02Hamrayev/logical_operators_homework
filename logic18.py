19e4c9a1c924d794dd70c4375afa6e19f1cf38f1
Navruzbek02Hamrayev <hamrayevnavruz12@gmail.com>
2023-09-29 14:52:48 -0400

    done by Navruzbek

--- logic18.py
+++ logic18.py
@@ -5,4 +5,6 @@ def main(a):
     Returns:
         bool: answer
     """
-    return
+    return (a%10)>(a//10%10) and (a//10%10)>(a//100%10) and (a//100%10)>(a//1000%10) and (a//1000%10)>(a//10000)
+print(main(75421))
+print(main(12347))
